f38bc1be64aff102259ebe647268d8390359926f
samir <samirchar@msn.com>
2020-04-21 00:26:40 -0500

    defined save function in TextProcessor. Defined a filter by text length function.

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from dotenv import load_dotenv, find_dotenv
-
+import numpy as np
 sys.path.append(os.path.abspath("../.."))
 # find .env automagically by walking up directories until it's found
 dotenv_path = find_dotenv()
@@ -16,6 +16,7 @@ from src.code_snippets.dataprep.cleaner import (count_characters_in_tokenized_se
                                                 count_punctuations_in_tokenized_sentece,
                                                 count_ner_tags_in_tokenized_sentence)
 from src.code_snippets.dataprep.embeddings_preprocessing.data_preparation import sentences_to_indices, pretrained_embedding_layer
+from src.code_snippets.utils.data_handler import read_pickle,save_to_pickle
 
 #TODO: Change this import to modin
 import pandas as pd
@@ -27,6 +28,7 @@ import re
 import gensim
 import string
 from typing import Tuple
+from sklearn.preprocessing import StandardScaler
 
 
 class textProcessor(DataProcessor):
@@ -40,16 +42,28 @@ class textProcessor(DataProcessor):
         self.server = serverConnection()
         self.server.start_server()
         
-    def read(self)->None:
+    def read(self, max_len: int = -1, text_col = None)->None:
         self.df = pd.read_csv(self.data_dir,nrows = self.nrows)
         if self.embedding_dir is not None:
             self.gensim_model = read_glove_file(self.embedding_dir)
             self.word_to_index,self.index_to_words = get_word_index_dicts(self.gensim_model)
-    
-    def clean(self,text_col: str,new_col_name: str)->None:
+        if max_len>0:
+            assert text_col is not None, 'Please specify the name of the text column'
+            self.filter_by_text_length(text_col,max_len)
+        self.max_len = max_len
+
+    def filter_by_text_length(self,text_col: str,max_len: int):
+        temp = self.df[text_col].apply(lambda x: len(word_tokenize(re.sub("\\n"," ",x))))
+        self.df = self.df[temp<=max_len]
+
+    def clean(self,text_col: str,new_col_name: str,post_processing_max_len = None)->None:
         st = CoreNLPParser()
         self.df[new_col_name] = self.df[text_col].apply(preprocessing)
         self.df[new_col_name] = self.df[new_col_name].apply(lambda text: list(st.tokenize(text)))
+        if post_processing_max_len is None:
+            self.post_processing_max_len = self.df[new_col_name].apply(len).max()
+        else:
+            self.post_processing_max_len = post_processing_max_len
         
     def feature_engineer(self,text_col: str)-> None:
         temp = self.df[text_col].apply(lambda x: re.sub("\\n"," ",x))
@@ -83,22 +97,34 @@ class textProcessor(DataProcessor):
             apply(lambda x: count_ner_tags_in_tokenized_sentence(x,'VERB'))\
             *100/self.df['num_words']
         
-    def pre_process(self,text_col: str,max_len: int, target_cols: list,aux_cols  = None, filter_long_texts = True):
-        
-        if filter_long_texts:
-            self.df = self.df[self.df['num_words']<=max_len]
-            max_len = self.df[text_col].apply(len).max()
+    def pre_process(self,text_col: str,max_len: int, target_cols: list,aux_cols  = None, standard_scaler = None):
         X = self.df[text_col].values
         X_indices = sentences_to_indices(X,self.word_to_index,max_len)
         y = self.df[target_cols].values
+        self.processed_data = { 'X_indices':X_indices, 'y':y}
         if aux_cols is not None:
             X_aux = self.df[aux_cols].values
-            return X_indices, X_aux, y
-        else:
-            return X_indices, y
-    
-    def save(self):
-        pass
+
+            #Standarize aux features. If given a standard scaler in params, this is used to transformed data.
+            #For this to work the standard scaler given as parameter must be previously trained. If no standard scaler is given,
+            #It is assumed that this is training data and we need to fit for the first time.
+            if standard_scaler is not None:
+                self.ss = standard_scaler
+                X_aux = self.ss.transform(X_aux)
+            else:
+                self.ss = StandardScaler()
+                self.ss.fit(X_aux)
+                X_aux = self.ss.transform(X_aux)
+
+            #Store X_aux to precessed data dictionary
+            self.processed_data['X_aux'] = X_aux
         
+    
+    def save(self,suffix,directory = "../../data/processed/"):
+        save_to_pickle(self.processed_data,os.path.join(directory,f"processed_data_{suffix}.pickle"))
+        save_to_pickle(self.ss,os.path.join(directory,f"StandardScaler_{suffix}.pickle"))
+        save_to_pickle(self.max_len,os.path.join(directory,f"max_len_{suffix}.pickle"))
+        save_to_pickle(self.post_processing_max_len,os.path.join(directory,f"post_processing_max_len_{suffix}.pickle"))
+            
         
     
