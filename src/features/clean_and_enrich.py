1dda8e4b483ecd5538e9414db55e715c2642b224
samir <samirchar@msn.com>
2020-04-21 00:26:40 -0500

    defined save function in TextProcessor. Defined a filter by text length function.

--- src/features/clean_and_enrich.py
+++ src/features/clean_and_enrich.py
@@ -10,42 +10,46 @@ load_dotenv(dotenv_path)
 # import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 import argparse
-import pandas as pd
+import modin.pandas as pd
+from src.code_snippets.utils.data_handler import read_pickle
 
 if __name__ == "__main__":
-    from src.features.build_features import textProcessor
 
+    from src.features.build_features import textProcessor
     parser = argparse.ArgumentParser()
     parser.add_argument("--max_len", type=int, default=3000)
     parser.add_argument("--get_splits", type=bool, default=False)
     parser.add_argument("--test_size", type=float, default=0.04)
     parser.add_argument("--nrows", type=int, default=-1)
     parser.add_argument("--embedding_path",type=str,default = "../../../../pretrained_embeddings/glove.twitter.27B/glove.twitter.27B.25d.txt")
+    parser.add_argument("--processed_data_path",type=str,default = "../../data/processed")
+    parser.add_argument("--raw_data_path",type=str,default = "../../data/raw")
+    parser.add_argument("--interim_data_path",type=str,default = "../../data/interim")
 
     args = parser.parse_args()
     if args.nrows == -1:
         args.nrows = None
     if args.get_splits:
-        df = pd.read_csv("../../data/raw/train.csv")
+        df = pd.read_csv(os.path.join(args.raw_data_path,"train.csv"))
         df_train, df_val = train_test_split(
             df, test_size=args.test_size, random_state=100
         )
-        df_train.to_csv("../../data/interim/train.csv", index=False)
-        df_val.to_csv("../../data/interim/val.csv", index=False)
+        df_train.to_csv(os.path.join(args.interim_data_path,"train.csv"), index=False)
+        df_val.to_csv(os.path.join(args.interim_data_path,"val.csv"), index=False)
     
     ### TRAIN ###
     tp = textProcessor(
-        data_dir="../../data/interim/train.csv",
+        data_dir=os.path.join(args.interim_data_path,"train.csv"),
         # TODO: Change embedding size
         embedding_dir=args.embedding_path,
         nrows=args.nrows,
     )
 
-    tp.read()
+    tp.read(max_len=args.max_len, text_col="comment_text")
     tp.clean("comment_text", "comment_cleaned")
     tp.feature_engineer("comment_text")
+    print(f"Max length after post processing is {tp.post_processing_max_len}")
     tp.df["y"] = (~tp.df["clean"].astype(bool)).astype(int).values
 
     aux_cols = [
@@ -63,40 +67,38 @@ if __name__ == "__main__":
     ]
 
     print(f"max comment length is {args.max_len}")
-    (X_indices_train, X_aux_train, y_train) = tp.pre_process(
-        "comment_cleaned", max_len=args.max_len, aux_cols=aux_cols, target_cols=["y"]
+    tp.pre_process(
+        "comment_cleaned", max_len=tp.post_processing_max_len, aux_cols=aux_cols, target_cols=["y"]
     )
 
-    ss = StandardScaler()
-    ss.fit(X_aux_train)
-    X_aux_train = ss.transform(X_aux_train)
+    #Kill corenlp server
     tp.server.kill_server()
 
     # Save
-    np.save("../../data/processed/X_indices_train.npy", X_indices_train)
-    np.save("../../data/processed/X_aux_train.npy", X_aux_train)
-    np.save("../../data/processed/y_train.npy", y_train)
+    tp.save('train')
 
 
     ### VALIDATION ###
+    StandardScaler_train = read_pickle(os.path.join(args.processed_data_path,"StandardScaler_train.pickle"))
+    max_len_train = read_pickle(os.path.join(args.processed_data_path,"max_len_train.pickle"))
+    post_processing_max_len_train = read_pickle(os.path.join(args.processed_data_path,"post_processing_max_len_train.pickle"))
+
     tp = textProcessor(
-        data_dir="../../data/interim/val.csv",
+        data_dir=os.path.join(args.interim_data_path,"val.csv"),
         embedding_dir=args.embedding_path,
         nrows=args.nrows,
     )
 
-    tp.read()
-    tp.clean("comment_text", "comment_cleaned")
+    tp.read(max_len=max_len_train, text_col="comment_text")
+    tp.clean("comment_text", "comment_cleaned",post_processing_max_len=post_processing_max_len_train)
     tp.feature_engineer("comment_text")
     tp.df["y"] = (~tp.df["clean"].astype(bool)).astype(int).values
 
-    (X_indices_val, X_aux_val, y_val) = tp.pre_process(
-        "comment_cleaned", max_len=args.max_len, aux_cols=aux_cols, target_cols=["y"]
+    tp.pre_process(
+        "comment_cleaned", max_len=tp.post_processing_max_len, aux_cols=aux_cols, target_cols=["y"],standard_scaler=StandardScaler_train
     )
-    X_aux_val = ss.transform(X_aux_val)
+    #Kill corenlp server
     tp.server.kill_server()
 
-    # Validation
-    np.save("../../data/processed/X_indices_val.npy", X_indices_val)
-    np.save("../../data/processed/X_aux_val.npy", X_aux_val)
-    np.save("../../data/processed/y_val.npy", y_val)
+    # save
+    tp.save('val')
