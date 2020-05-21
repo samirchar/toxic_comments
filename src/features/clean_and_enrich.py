2bb3887f71256acfa2a3279aef210b7fb3451200
samir <samirchar@msn.com>
2020-05-21 10:20:02 -0500

    modified clean_and_enrich. Now it supports different modes depending on wether we want to transform or fit and transform an input file

--- src/features/clean_and_enrich.py
+++ src/features/clean_and_enrich.py
@@ -18,6 +18,9 @@ if __name__ == "__main__":
 
     from src.features.build_features import textProcessor
     parser = argparse.ArgumentParser()
+    parser.add_argument("--file_name",type=str)
+    parser.add_argument("--file_source_path",type=str,help="Available sources are: 'raw','interim'")
+    parser.add_argument("--mode",type=str,help="Available modes are transform or fit_transform")
     parser.add_argument("--max_len", type=int, default=115)
     parser.add_argument("--get_splits", type=bool, default=False)
     parser.add_argument("--test_size", type=float, default=0.05)
@@ -26,34 +29,6 @@ if __name__ == "__main__":
     parser.add_argument("--processed_data_path",type=str,default = "../../data/processed")
     parser.add_argument("--raw_data_path",type=str,default = "../../data/raw")
     parser.add_argument("--interim_data_path",type=str,default = "../../data/interim")
-
-    args = parser.parse_args()
-    if args.nrows == -1:
-        args.nrows = None
-    if args.get_splits:
-        df = pd.read_csv(os.path.join(args.raw_data_path,"train.csv"))
-        df_train, df_val = train_test_split(
-            df, test_size=args.test_size, random_state=100
-        )
-        df_train.to_csv(os.path.join(args.interim_data_path,"train.csv"), index=False)
-        df_val.to_csv(os.path.join(args.interim_data_path,"val.csv"), index=False)
-    
-    ### TRAIN ###
-    print("=====================================================================================================")
-    print("=================================== PROCESS TRAINING  ===============================================")
-    print("=====================================================================================================")
-
-    tp = textProcessor(
-        data_dir=os.path.join(args.interim_data_path,"train.csv"),
-        # TODO: Change embedding size
-        embedding_dir=args.embedding_path,
-        nrows=args.nrows,
-    )
-
-    tp.read()
-    tp.clean("comment_text", "comment_cleaned")
-    tp.feature_engineer("comment_text")
-    tp.df["y"] = (~tp.df["clean"].astype(bool)).astype(int).values
 
     aux_cols = [
         "num_characters",
@@ -69,27 +44,27 @@ if __name__ == "__main__":
         "verbs_pct",
     ]
 
-    print(f"max comment length is {args.max_len}")
-    tp.pre_process(
-        "comment_cleaned", max_len=args.max_len, aux_cols=aux_cols, target_cols=["y"],filter_by_max_length = True
-    )
+    args = parser.parse_args()
+    
+    if args.file_source_path == 'raw':
+        file_source = args.raw_data_path
+    elif args.file_source_path == 'interim':
+        file_source = args.interim_data_path
+    else:
+        print("This file source is not available")
 
-    #Kill corenlp server
-    tp.server.kill_server()
-
-    # Save
-    tp.save('train')
-
-
-    ### VALIDATION ###
-    print("=====================================================================================================")
-    print("=================================== PROCESS VALIDATION  =============================================")
-    print("=====================================================================================================")
-
-    StandardScaler_train = read_pickle(os.path.join(args.processed_data_path,"StandardScaler_train.pickle"))
-
+    if args.nrows == -1:
+        args.nrows = None
+    if args.get_splits:
+        df = pd.read_csv(os.path.join(args.raw_data_path,"train.csv"))
+        df_train, df_val = train_test_split(
+            df, test_size=args.test_size, random_state=100
+        )
+        df_train.to_csv(os.path.join(args.interim_data_path,"train.csv"), index=False)
+        df_val.to_csv(os.path.join(args.interim_data_path,"val.csv"), index=False)
+    
     tp = textProcessor(
-        data_dir=os.path.join(args.interim_data_path,"val.csv"),
+        data_dir=os.path.join(file_source,args.file_name),
         embedding_dir=args.embedding_path,
         nrows=args.nrows,
     )
@@ -99,11 +74,26 @@ if __name__ == "__main__":
     tp.feature_engineer("comment_text")
     tp.df["y"] = (~tp.df["clean"].astype(bool)).astype(int).values
 
-    tp.pre_process(
-        "comment_cleaned", max_len=args.max_len, aux_cols=aux_cols, target_cols=["y"],standard_scaler=StandardScaler_train,filter_by_max_length = True
-    )
+
+    print(f"max comment length is {args.max_len}")
+
+    if args.mode == "fit_transform":
+        tp.pre_process(
+            "comment_cleaned", max_len=args.max_len, aux_cols=aux_cols, target_cols=["y"],filter_by_max_length = True
+        )
+
+    elif args.mode == "transform":
+
+        StandardScaler_train = read_pickle(os.path.join(args.processed_data_path,"StandardScaler_train.pickle"))
+
+        tp.pre_process(
+            "comment_cleaned", max_len=args.max_len, aux_cols=aux_cols, target_cols=["y"],standard_scaler=StandardScaler_train,filter_by_max_length = True
+        )
+    else:
+        print(f"Mode {args.mode} is not supported. Available modes are: 'transform', 'fit_transform' ")
+
     #Kill corenlp server
     tp.server.kill_server()
 
-    # save
-    tp.save('val')
+    # Save
+    tp.save('.'.join(args.file_name.split('.')[0:-1]))
